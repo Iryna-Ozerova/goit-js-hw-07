7880cdb468dd2bedf01b733714a3a642edd03c3e
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-02-08 12:52:14 +0200

    add hw-7

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,18 @@
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+
+button.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.style.color = randomColor;
+  span.textContent = randomColor;
+});
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
