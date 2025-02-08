a8a89b28d4d1d4e467763414362c130627b157b2
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-02-08 10:27:27 +0200

    update task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,6 @@
-const input = document.querySelector("#name-input");
+const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
-function updateNameOutput() {
-    const trimmedValue = input.value.trim();
-
-    if (trimmedValue === "") {
-        output.textContent = "Anonymous";
-    }
-    else {
-        output.textContent = trimmedValue;
-    }
-}
-
-input.addEventListener("input", updateNameOutput);
+textInput.addEventListener("input", (event) => {
+  output.textContent = event.currentTarget.value.trim() || "Anonymous";
+});
